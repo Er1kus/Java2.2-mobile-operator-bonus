09a9225037739ac2f45aa2403f3f9128fb983708
Er1kus <goryaev86@mail.ru>
2022-04-25 23:53:31 +0300

    Mobile operator bonus calculation

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int account = 500;
+        int payment = 1200;
+        int bonusCost = 100;
+        int secondPayment = 3000;
+
+        int bonus;
+        if (payment >= 1000) {
+            bonus = payment / bonusCost;
+        } else {
+            bonus = 0;
+        }
+        int sumAccount = account + payment + bonus;
+
+        int bonus2;
+        if (secondPayment >= 1000) {
+            bonus2 = secondPayment / bonusCost;
+        } else {
+            bonus2 = 0;
+        }
+        int sumAccount2 = account + secondPayment + bonus2;
+        System.out.println("Начисленные бонусы за пополнение: " + bonus);
+        System.out.println("Итоговый счёт после пополнения: " + sumAccount);
+        System.out.println("Начисленные бонусы за последующее пополнение: " + bonus2);
+        System.out.println("Итоговый счёт после последующего пополнения: " + sumAccount2);
+
+    }
+}
